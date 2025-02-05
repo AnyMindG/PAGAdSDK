edefc6ab007bca236b2adf7516661aa673747b4e
narender <111370321+AnyMindG@users.noreply.github.com>
2025-02-05 20:52:18 +0530

    Updating Swift Package to 1.4.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-// v1.3.0
+// v1.4.0
 let package = Package(
   name: "PAGAdSDK",
   defaultLocalization: "en",
@@ -16,22 +16,37 @@ let package = Package(
   ],
   targets: [
     .binaryTarget(
-      name: "PAGAdSDK",
+      name: "PAGAdSDKBinary",
       url: "https://github.com/AnyMindG/AllFrameworks/releases/download/10000.3.0/PAGAdSDK.zip", // Add actual URL
       checksum: "c6363d4f71824814089dd40be94448484e73d930ecb0e18e7586d03f9dcbda35" // Add actual checksum
     ),
-    // comment out
     
-//    .target(
-//      name: "PAGAdSDK",
-//      dependencies: [],
-//      exclude: [
-//        "Resources/Info.plist" // Exclude specific file
-//      ],
-//      resources: [
-//        .process("Resources") // Process the Resources folder
-//      ]
-//    ),
+    .target(
+      name: "PAGAdSDK",
+      dependencies: [],
+      exclude: [
+        "Resources/Info.plist" // Exclude specific file
+      ],
+      resources: [
+        .process("Resources") // Process the Resources folder
+      ],
+      linkerSettings: [
+          .linkedFramework("WebKit"),
+          .linkedFramework("AdSupport"),
+          .linkedFramework("AVFoundation"),
+          .linkedFramework("CoreGraphics"),
+          .linkedFramework("CoreMedia"),
+          .linkedFramework("CoreTelephony"),
+          .linkedFramework("StoreKit"),
+          .linkedFramework("SystemConfiguration"),
+          .linkedFramework("UIKit"),
+          .linkedFramework("AudioToolbox"),
+          .linkedFramework("CFNetwork"),
+          .linkedFramework("MediaPlayer"),
+          .linkedFramework("QuartzCore"),
+          .linkedFramework("CoreAudioTypes"),
+      ]
+    ),
     .testTarget(
       name: "PAGAdSDKTests",
       dependencies: ["PAGAdSDK"]
