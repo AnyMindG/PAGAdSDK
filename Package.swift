dbd3d65710af11ec5bdbe2acb81e72937fea0906
narender <111370321+AnyMindG@users.noreply.github.com>
2025-02-05 14:26:42 +0530

    Updating Swift Package to 1.1.0

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+let package = Package(
+  name: "PAGAdSDK",
+  defaultLocalization: "en",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(
+      name: "PAGAdSDK",
+      targets: ["PAGAdSDK"]
+    ),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "PAGAdSDKBinary",
+      url: "https://github.com/AnyMindG/AllFrameworks/releases/download/10000.3.0/PAGAdSDK.zip", // Add actual URL
+      checksum: "c6363d4f71824814089dd40be94448484e73d930ecb0e18e7586d03f9dcbda35" // Add actual checksum
+    ),
+    .target(
+      name: "PAGAdSDK",
+      dependencies: [],
+      exclude: [
+        "Resources/Info.plist" // Exclude specific file
+      ],
+      resources: [
+        .process("Resources") // Process the Resources folder
+      ]
+    ),
+    .testTarget(
+      name: "PAGAdSDKTests",
+      dependencies: ["PAGAdSDK"]
+    ),
+  ]
+)
